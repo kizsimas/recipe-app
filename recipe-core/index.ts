82431de0cb8b7ae1bae45ed26f4a0860ba080f09
SimasKi <simaskizys@gmail.com>
2022-10-17 10:15:57 +0300

    add swagger, update packages

--- recipe-core/index.ts
+++ recipe-core/index.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import recipeController from './recipes/recipes.controller';
 import unitController from './units/units.controller';
+import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
 
+const swaggerFile = yaml.load('./swagger.yaml');
 dotenv.config();
 
 const app = express();
@@ -10,6 +13,12 @@ const port = process.env.SERVER_PORT;
 
 app.use("/recipes", recipeController);
 app.use("/units", unitController);
+
+app.use(
+  "/swagger",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile)
+);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
---
+++ recipe-core/prisma/migrations/20221017065120_migration/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `unitCategory` on the `Unit` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Unit" DROP COLUMN "unitCategory";
