d5df71c33e3c5aac377df64b617212689ed23c07
Simas Kizys <simaskizys@gmail.com>
2024-07-13 11:49:59 +0300

    dogs

--- recipe-web/pages/index.tsx
+++ recipe-web/pages/index.tsx
@@ -13,6 +13,14 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <div className={styles.dogs}>
+          <img className={styles.suo} src="http://192.168.1.8/images/so.png" alt="" />
+          <img className={styles.suo} src="http://192.168.1.8/images/suo11.PNG" alt="" />
+          <img className={styles.suo} src="http://192.168.1.8/images/suo12.PNG" alt="" />
+          <img className={styles.suo} src="http://192.168.1.8/images/suo13.PNG" alt="" />
+          <img className={styles.suo} src="http://192.168.1.8/images/suo14.PNG" alt="" />
+          <img className={styles.suo} src="http://192.168.1.8/images/suo15.PNG" alt="" />
+        </div>
         <h1 className="text-3xl font-bold underline text-blue-500">
           Cool recipe app
         </h1>
